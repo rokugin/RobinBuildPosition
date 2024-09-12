5730aa54d247393ed0657985694882cb2dcdc1e5
rokugin <rokugin@gmail.com>
2024-09-12 13:21:40 -0700

    Add outdoor position offsetting

---
+++ Patches/NPCPatch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.GameData.Buildings;
+
+namespace RobinBuildPosition.Patches;
+
+[HarmonyPatch(typeof(NPC), "updateConstructionAnimation")]
+static class NPCPatch {
+
+    static void Postfix(NPC __instance) {
+        if (Game1.IsThereABuildingUnderConstruction()) {
+            Building b = Game1.GetBuildingUnderConstruction();
+            BuildingData data = b.GetData();
+            int X = 1, Y = 5;
+
+            if (data != null && data.CustomFields != null && data.CustomFields.TryGetValue("rokugin.robinbuildposition", out string? value)) {
+                string[] position = value!.Split(" ");
+                if (!Int32.TryParse(position[0], out X)) {
+                    X = 1;
+                }
+                if (!Int32.TryParse(position[1], out Y)) {
+                    Y = 5;
+                }
+            }
+
+            if (b.daysUntilUpgrade.Value > 0 && b.GetIndoors() != null) {
+                __instance.setTilePosition(X, Y);
+            } else {
+                (__instance).position.X += X;
+                (__instance).position.Y += Y;
+            }
+        }
+    }
+
+}
+
